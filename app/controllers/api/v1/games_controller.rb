e84e88b5c9965a073aaa871912c39753f41699e1
Chris Powell <chris.ian.powell@gmail.com>
2018-09-12 12:07:07 -0600

    I think I did the whole thing

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -2,4 +2,9 @@ class Api::V1::GamesController < ApplicationController
   def show
     render json: Game.find(params[:id])
   end
+
+  def update
+    Play.create(user_id: request.headers[:user_id], game_id: params[:id], word:request.headers[:word])
+    render json: Game.find(params[:id])
+  end
 end
--- spec/requests/api/v1/games_spec.rb
+++ spec/requests/api/v1/games_spec.rb
@@ -24,4 +24,25 @@ describe 'JSON api output for specific game' do
       expect(game_api[:scores][1][:score]).to eq(play2.score)
     end
   end
+  context "post /api/v1/games/1/plays" do
+    it 'updates game as a json object' do
+      josh = User.create(id: 1)
+      sal  = User.create(id: 2)
+
+      game = Game.create(id: 1, player_1: josh, player_2: sal)
+
+      play1 = josh.plays.create(game: game, word: "qwa", score: 15)
+      play2 = sal.plays.create(game: game, word: "qwat", score: 16)
+
+      post "/api/v1/games/1/plays", headers: { 'user_id' => 1, 'word' => 'at'}
+
+      game_api = JSON.parse(response.body, symbolize_names: true)
+
+      expect(game_api[:game_id]).to eq(game.id)
+      expect(game_api[:scores][0][:user_id]).to eq(josh.id)
+      expect(game_api[:scores][0][:score]).to eq(play1.score + 2)
+      expect(game_api[:scores][1][:user_id]).to eq(sal.id)
+      expect(game_api[:scores][1][:score]).to eq(play2.score)
+    end
+  end
 end
