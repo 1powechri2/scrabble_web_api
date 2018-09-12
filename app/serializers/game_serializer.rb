85077da30603522480f8d723e078e9383ebff58f
Chris Powell <chris.ian.powell@gmail.com>
2018-09-12 11:06:17 -0600

    user story 2 complete

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,3 +1,14 @@
 class GameSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :game_id, :scores
+
+  def game_id
+    object.id
+  end
+
+  def scores
+    [{user_id: object.player_1.id,
+       score: object.player_1.plays.sum(:score)},
+     {user_id: object.player_2.id,
+       score: object.player_2.plays.sum(:score)}]
+  end
 end
--- spec/requests/api/v1/games_spec.rb
+++ spec/requests/api/v1/games_spec.rb
@@ -8,26 +8,20 @@ describe 'JSON api output for specific game' do
 
       game = Game.create(id: 1, player_1: josh, player_2: sal)
 
-      josh.plays.create(game: game, word: "qwa", score: 15)
-      sal.plays.create(game: game, word: "qwat", score: 16)
+      play1 = josh.plays.create(game: game, word: "qwa", score: 15)
+      play2 = sal.plays.create(game: game, word: "qwat", score: 16)
 
       get '/api/v1/games/1'
 
       expect(response.status).to eq(200)
-      
-              {
-          "game_id":1,
-          "scores": [
-            {
-              "user_id":1,
-              "score":15
-            },
-            {
-              "user_id":2,
-              "score":16
-            }
-          ]
-        }
+
+      game_api = JSON.parse(response.body, symbolize_names: true)
+
+      expect(game_api[:game_id]).to eq(game.id)
+      expect(game_api[:scores][0][:user_id]).to eq(josh.id)
+      expect(game_api[:scores][0][:score]).to eq(play1.score)
+      expect(game_api[:scores][1][:user_id]).to eq(sal.id)
+      expect(game_api[:scores][1][:score]).to eq(play2.score)
     end
   end
 end
